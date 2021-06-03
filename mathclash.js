a7b9864283670a7e53fba8a9398feec87f53eed3
nareshsolanki99 <nareshsolanki3456@gmail.com>
2021-06-03 14:20:09 +0530

    Update mathclash.js

--- mathclash.js
+++ mathclash.js
@@ -7,114 +7,109 @@ var startCell;
 var endCell;
 var result;
 var cellClickCount;
-function startGame(){
-    sum = 0;
-    result = 0;
-    initialTime = 12;
-    count = initialTime;
-    cellClickCount = 0;
-    startCell = 1;
-    endCell = 12;
-    round = 0;
-    randomNum = Math.floor(Math.random() * 50) + 1;
-    console.log("startgame endcell "+endCell);
-    generateRandomNum(startCell,endCell);
-    document.getElementById("number-block").innerHTML = randomNum;
-    timer = setInterval(starttimer,1000);
+function startGame() {
+  sum = 0;
+  result = 0;
+  initialTime = 12;
+  count = initialTime;
+  cellClickCount = 0;
+  startCell = 1;
+  endCell = 12;
+  round = 0;
+  randomNum = Math.floor(Math.random() * 50) + 1;
+  generateRandomNum(startCell, endCell);
+  document.getElementById("number-block").innerHTML = randomNum;
+  timer = setInterval(starttimer, 1000);
 }
 
-function generateRandomNum(startCell,endCell){
-    for(var i=startCell;i<=endCell;i++){
-        var newRandomNum = Math.floor(Math.random() * 9) + 1;
-        document.getElementById("cell"+i).innerHTML = newRandomNum;
-    }
+function generateRandomNum(startCell, endCell) {
+  for (var i = startCell; i <= endCell; i++) {
+    var newRandomNum = Math.floor(Math.random() * 9) + 1;
+    document.getElementById("cell" + i).innerHTML = newRandomNum;
+  }
 }
 
-function displayRandomNum(){
-    randomNum = Math.floor(Math.random() * 50) + 1;
-    document.getElementById("number-block").innerHTML = randomNum;
+function displayRandomNum() {
+  randomNum = Math.floor(Math.random() * 50) + 1;
+  document.getElementById("number-block").innerHTML = randomNum;
 }
 
-function starttimer(){
-       console.log("end-cell "+endCell);
-        if (count == 0) {
-            clearInterval(timer);
-        count = initialTime<4?initialTime:initialTime--;
-          sum = 0;
-          if (endCell == 36) {
-            document.getElementById("score-label").innerHTML = result;
-            document.getElementById("result-container").style.visibility =
-              "visible";
-          }else {
-            round++;
-            endCell += 6;
-            clearCells();
-            displayRandomNum();
-            generateRandomNum(startCell, endCell);
-            document.getElementById("sum-block").innerHTML = sum;
-            timer = setInterval(starttimer, 1000);
-          }
-        }
-       count--;
-       document.getElementById("timer").innerHTML = count;   
-}
-
-function sumUp(cellId){
-    var cell = document.getElementById("cell"+cellId);
-    var numString = cell.innerHTML;
-    var num = Number(numString);
-    if(cell.getAttribute('set') == 1){
-        if(sum>0)
-        sum -= num;
-        cellClickCount--;
-        cell.setAttribute('set',0);
-        cell.style.backgroundColor = "#fff5fd"
-    }else{
-        sum += num;
-        cell.setAttribute('set',1);
-        cell.style.backgroundColor = "#0a81ab"
-        cellClickCount++;
-    }
-    document.getElementById("sum-block").innerHTML = sum;
-    if(sum > randomNum){
-        endCell += 6;
-    }else if(sum == randomNum){
-        if(endCell>12){
-            endCell -= 6;
-        }
-        result += cellClickCount;
-        document.getElementById("score").innerHTML = result;
-        document.getElementById("timer").innerHTML = count;
-    }else{
-        console.log("Inside else of sumUp");
-        return
-    }
-    count = initialTime<4?initialTime:initialTime--;
-    document.getElementById("timer").innerHTML = count;
-    clearCells();
-    displayRandomNum();
-    generateRandomNum(startCell,endCell);
+function starttimer() {
+  if (count == 0) {
     clearInterval(timer);
-    timer = setInterval(starttimer, 1000);
-    document.getElementById("sum-block").innerHTML = sum;
-}
-
-function clearCells(){
-    for(var i=1;i<=36;i++){
-        var cell = document.getElementById("cell"+i); 
-        cell.innerHTML = "";
-        cell.setAttribute("set",0);
-        cell.style.backgroundColor = "#fff5fd";
-        cellClickCount = 0;
-        sum = 0;
+    count = initialTime < 4 ? initialTime : initialTime--;
+    sum = 0;
+    if (endCell == 36) {
+      document.getElementById("score-label").innerHTML = result;
+      document.getElementById("result-container").style.visibility = "visible";
+    } else {
+      round++;
+      endCell += 6;
+      clearCells();
+      displayRandomNum();
+      generateRandomNum(startCell, endCell);
+      document.getElementById("sum-block").innerHTML = sum;
+      timer = setInterval(starttimer, 1000);
     }
+  }
+  count--;
+  document.getElementById("timer").innerHTML = count;
 }
 
-function replayGame(){
-    document.getElementById("result-container").style.visibility = "hidden";
-    clearCells();
-    startGame();
+function sumUp(cellId) {
+  var cell = document.getElementById("cell" + cellId);
+  var numString = cell.innerHTML;
+  var num = Number(numString);
+  if (cell.getAttribute("set") == 1) {
+    if (sum > 0) sum -= num;
+    cellClickCount--;
+    cell.setAttribute("set", 0);
+    cell.style.backgroundColor = "#fff5fd";
+  } else {
+    sum += num;
+    cell.setAttribute("set", 1);
+    cell.style.backgroundColor = "#0a81ab";
+    cellClickCount++;
+  }
+  document.getElementById("sum-block").innerHTML = sum;
+  if (sum > randomNum) {
+    endCell += 6;
+  } else if (sum == randomNum) {
+    if (endCell > 12) {
+      endCell -= 6;
+    }
+    result += cellClickCount;
+    document.getElementById("score").innerHTML = result;
+    document.getElementById("timer").innerHTML = count;
+  } else {
+    return;
+  }
+  count = initialTime < 4 ? initialTime : initialTime--;
+  document.getElementById("timer").innerHTML = count;
+  clearCells();
+  displayRandomNum();
+  generateRandomNum(startCell, endCell);
+  clearInterval(timer);
+  timer = setInterval(starttimer, 1000);
+  document.getElementById("sum-block").innerHTML = sum;
 }
-function closeGame(){
-    document.getElementById("result-container").style.visibility = "hidden";
+
+function clearCells() {
+  for (var i = 1; i <= 36; i++) {
+    var cell = document.getElementById("cell" + i);
+    cell.innerHTML = "";
+    cell.setAttribute("set", 0);
+    cell.style.backgroundColor = "#fff5fd";
+    cellClickCount = 0;
+    sum = 0;
+  }
+}
+
+function replayGame() {
+  document.getElementById("result-container").style.visibility = "hidden";
+  clearCells();
+  startGame();
+}
+function closeGame() {
+  document.getElementById("result-container").style.visibility = "hidden";
 }
